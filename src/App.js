6c5cc295ff8f4a8c2d15a44ff116d3d0637097a1
Heather Peterson <hpeterson462@gmail.com>
2021-03-14 18:27:56 -0700

    right drawer form w/ radio btns

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import markerIconPng from 'leaflet/dist/images/marker-icon.png';
 import { Icon } from 'leaflet';
+import 'esri-leaflet-geocoder/dist/esri-leaflet-geocoder.css';
 import LocationMarker from './LocationMarker';
-import Menu from './Menu';
+import Form from './Form';
 import daycaresData from './data/daycaresData';
 import './App.css';
 
@@ -12,8 +13,7 @@ export default function App() {
   const daycareElements = daycaresData.map(daycare => (
     <Marker key={daycare.id} position={[daycare.lat, daycare.lng]} icon={new Icon({ iconUrl: markerIconPng, iconSize: [25, 41], iconAnchor: [12, 41] })}>
       <Popup>
-        {daycare.name}
-        <br />
+        <h4>{daycare.name}</h4>
         {daycare.streetAddress}
         <br />
         {daycare.city}, {daycare.state}
@@ -23,8 +23,9 @@ export default function App() {
 
   return (
     <>
+      <Form />
       <h3>Click the map to find your location.</h3>
-      <MapContainer className="map" center={{ lat: 47.608013, lng: -122.335167 }} zoom={13} scrollWheelZoom={true}>
+      <MapContainer className="map" center={{ lat: 47.608013, lng: -122.335167 }} zoom={13} scrollWheelZoom={false}>
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -32,7 +33,6 @@ export default function App() {
         <LocationMarker />
         {daycareElements}
       </MapContainer>
-      <Menu />
     </>
   )
 }
