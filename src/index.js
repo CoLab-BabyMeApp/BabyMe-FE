ab51c5b9a684202f8316e0bbd280bf1707ec7950
Heather Peterson <hpeterson462@gmail.com>
2021-03-18 00:13:58 -0700

    landing page

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'leaflet/dist/leaflet.css';
 import App from './App';
+import Landing from './Landing';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Landing />
   </React.StrictMode>,
   document.getElementById('root')
 );
