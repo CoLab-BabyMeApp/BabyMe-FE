255ceec37f7b83f7e95c27e4943b1a7cd1b5ddf3
Heather Peterson <hpeterson462@gmail.com>
2021-03-17 19:23:39 -0700

    scrollwheel on

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ export default function App() {
     <>
       <Form />
       <h3>Click the map to find your location.</h3>
-      <MapContainer className="map" center={{ lat: 47.608013, lng: -122.335167 }} zoom={13} scrollWheelZoom={false}>
+      <MapContainer className="map" center={{ lat: 47.608013, lng: -122.335167 }} zoom={13} scrollWheelZoom={true}>
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
